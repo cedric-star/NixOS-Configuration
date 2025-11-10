62a0de30b94aa646e150ce2e61f9b93590a0477f
cedric-star <cedric-star@gmail.com>
2025-11-10 12:34:27 +0100

    Auto-commit

--- system/system_programs/steam.nix
+++ system/system_programs/steam.nix
@@ -4,10 +4,10 @@
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true; #gamescope %command% (in general -> launch optinions)
   programs.gamemode.enable = true; #gamemoderun %command% (in general -> launch optinions)
-  
+
 
   environment.sessionVariables = {# für protonup: einmalig command "protonup" ins terminal
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/user/.steam/rot/compatibilitytools.d";
-  }
+  };
 
 }
