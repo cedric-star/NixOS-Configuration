d3c0d03679bb65a09017b5bee3f832c64960d0ea
cedric-star <cedric-star@gmail.com>
2025-11-10 12:19:11 +0100

    Auto-commit

--- home/programs/steam.nix
+++ home/programs/steam.nix
@@ -3,7 +3,7 @@
 {
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true; #gamescope %command% (in general -> launch optinions)
-  programs.hamemode.enable = true; #gamemoderun %command% (in general -> launch optinions)
+  programs.gamemode.enable = true; #gamemoderun %command% (in general -> launch optinions)
 
-  
+
 }
