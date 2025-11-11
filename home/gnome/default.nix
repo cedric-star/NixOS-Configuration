a68a312ebcfa4fbef943187051894c15487c439b
cedric-star <cedric-star@gmail.com>
2025-11-11 12:39:51 +0100

    Auto-commit

--- home/gnome/default.nix
+++ home/gnome/default.nix
@@ -43,8 +43,7 @@
       ];
     };
 
-    "org/gnome/shell/extensions/Logo-menu" = {
-      enable = true;
+    "org/gnome/shell/extensions/logo-menu" = {
       menu-button-icon-image = 23;
       menu-button-icon-size = 24;
       symbolic-icon = true;
