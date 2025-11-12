f973785af78cd3f76ec3b9bcd790b3b668eec35c
cedric-star <cedric-star@gmail.com>
2025-11-12 22:28:57 +0100

    Auto-commit

--- home/gnome/extensions/burnmywindows.nix
+++ home/gnome/extensions/burnmywindows.nix
@@ -8,7 +8,7 @@
   dconf.settings = {
     "org/gnome/shell" = {
       enabled-extensions = [ "burn-my-windows@schneegans.github.com "];
-      #disabled-extensions = [];
+      disabled-extensions = [];
     };
   ### Schließenanimation für alle Fenster
     #"org/gnome/shell/extensions/burn-my-windows" = {
