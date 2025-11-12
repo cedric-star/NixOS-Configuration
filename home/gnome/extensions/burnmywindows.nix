21e0923a4d21c2583324f321cffce1295bf91049
cedric-star <cedric-star@gmail.com>
2025-11-12 22:05:39 +0100

    Auto-commit

--- home/gnome/extensions/burnmywindows.nix
+++ home/gnome/extensions/burnmywindows.nix
@@ -6,7 +6,7 @@
   ];
 
   dconf.settings = {
-    "org/gnome/shell" = {enabled-extensions = [ "burn-my-windows@schneegans.github.com "]};
+    "org/gnome/shell" = {enabled-extensions = [ "burn-my-windows@schneegans.github.com "];};
   ### Schließenanimation für alle Fenster
     "org/gnome/shell/extensions/burn-my-windows" = {
       active-profile = "aura-glow";
