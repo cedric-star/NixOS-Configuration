340e73914fa7ff032b56621058a1f6a6552677e7
cedric-star <cedric-star@gmail.com>
2025-11-14 16:34:56 +0100

    Auto-commit

---
+++ home/gnome/extensions/openbar.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  dconf.settings = {
+    "org/gnome/shell/extensions/openbar" = {
+      autofg-bar = false;
+      bgalpha = 0;
+      bradius = 20;
+      bwidth = 0;
+      candy1 = ["1.000" "0.000" "0.561"];
+      candy2 = ["1.000" "0.000" "0.561"];
+      candy3 = ["1.000" "0.000" "0.561"];
+      candy4 = ["1.000" "0.000" "0.561"];
+      candy5 = ["1.000" "0.000" "0.561"];
+      candy6 = ["1.000" "0.000" "0.561"];
+
+      candyalpha = 0.5;
+      candybar = true;
+
+      dark-candy1 = ["1.000" "0.000" "0.561"];
+      dark-candy2 = ["1.000" "0.000" "0.561"];
+      dark-candy3 = ["1.000" "0.000" "0.561"];
+      dark-candy4 = ["1.000" "0.000" "0.561"];
+      dark-candy5 = ["1.000" "0.000" "0.561"];
+      dark-candy6 = ["1.000" "0.000" "0.561"];
+
+      fgcolor = ["0.122" "0.106" "0.106"];
+      font = "Adwaita Mono Bold 10";
+
+      height = 30;
+      hpad = 2.3
+    };
+  };
+}
