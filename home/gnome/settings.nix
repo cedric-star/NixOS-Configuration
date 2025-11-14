0e9d248ca676b59921642fc6852d7a93e65a7129
cedric-star <cedric-star@gmail.com>
2025-11-14 20:34:43 +0100

    Auto-commit

--- home/gnome/settings.nix
+++ home/gnome/settings.nix
@@ -12,7 +12,7 @@
   ];
 
   ### Hintergrundbild importieren
-  home.file.".local/share/wallpapers/b1.png".source = ./wallpapers/b1.png;
+  home.file.".local/share/wallpapers/b1.jpeg".source = ./wallpapers/b1.jpeg;
 
   dconf.settings = {
     "org/gnome/shell" = {
