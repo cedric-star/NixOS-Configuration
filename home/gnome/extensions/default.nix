2ad2f6c4f941d741c58991651bad802dec7f1236
cedric-star <cedric-star@gmail.com>
2025-11-16 14:41:12 +0100

    Auto-commit

--- home/gnome/extensions/default.nix
+++ home/gnome/extensions/default.nix
@@ -37,6 +37,7 @@
         "transparent-window-moving@noobsai.github.com" 
         "burn-my-windows@schneegans.github.com"
         "openbar@neuromorph"
+        "Vitals@CoreCoding.com"
       ];
     };
   };
