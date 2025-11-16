d3d8fe306e3a816979bf29b9904fb7042ce4e8fb
cedric-star <cedric-star@gmail.com>
2025-11-16 22:21:08 +0100

    Auto-commit

--- home/default.nix
+++ home/default.nix
@@ -19,6 +19,7 @@
   };
 
   programs.fish.enable = true;
+  shell = pkgs.fish;
 
 
   #programs.java.enable = true;
@@ -27,25 +28,9 @@
 
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+  home.file = { };
 
 
-  home.sessionVariables = {
-    TERMINAL = "kitty";
 
-  };
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -5,13 +5,12 @@
   home.file.".config/kitty/b1.png".source = ../gnome/wallpapers/b1.png;
   programs.kitty = {
     enable = true;
-    
+
+        
     
     settings = {
       background_opacity = 0.92;
       hide_window_decorations = "no";
-
-      
     };
     
     
