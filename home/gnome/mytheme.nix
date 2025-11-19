7865a3e8f27d494ee4a470582992c7a840e48283
cedric-star <cedric-star@gmail.com>
2025-11-19 10:43:18 +0100

    init my theme

--- home/gnome/mytheme.nix
+++ home/gnome/mytheme.nix
@@ -8,9 +8,14 @@ let
     src = ./themes/pink_clean;
 
     dontUnpack = true;
+    buildInputs = [ pkgs.sass ];
 
     installPhase = ''
       mkdir -p $out/share/themes/my-theme
+
+      echo "building css from scss..."
+      sass "$src/gnome-shell.scss" "$out/share/themes/my-theme/gnome-shell.css"
+
       cp -r $src/* $out/share/themes/my-theme/
     '';
   };
