91ae879c318a343ba699d92d1f44d8c03be8f5b4
cedric-star <cedric-star@gmail.com>
2025-11-24 23:50:22 +0100

    pywal + dependencies

--- home/gnome/setuppywal.nix
+++ home/gnome/setuppywal.nix
@@ -1,18 +1,17 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
-let pywal-nix = import (pkgs.fetchFromGitHub {
-  owner = "Fuwn";
-  repo = "pywal.nix";
-  rev = "main";
-  hash = "iQU1Rv7Q0BFdsIX9c7mxDhhYaWemuaNRYs+sR1DF0Rc=";
-});
-
-in {
+{
   imports = [
-    pywal-nix.homeManagerModules.${pkgs.system}.default
+    (import (
+      pkgs.fetchFromGitHub {
+        owner = "Fuwn";
+        repo = "pywal.nix";
+        rev = "main";  # Use the current commit revision hash
+        hash = "iQU1Rv7Q0BFdsIX9c7mxDhhYaWemuaNRYs+sR1DF0Rc="; # Use the current commit sha256 hash
+      }
+    )).homeManagerModules.${builtins.currentSystem}.default
   ];
-
-  pywal-nix = {
+    pywal-nix = {
     wallpaper = ../wallpapers/b1.png;
     light = false;
     backend = "wal";
