f4d1f6aff7742cc07f13cfbafe637f231ed51ea2
cedric-star <cedric-star@gmail.com>
2025-11-25 12:20:36 +0100

    pywal

--- home/gnome/setuppywal.nix
+++ home/gnome/setuppywal.nix
@@ -1,15 +1,4 @@
 { pkgs, ... }:
 
 {
-  imports = [
-    (import (
-      pkgs.fetchFromGitHub {
-        owner = "Fuwn";
-        repo = "pywal.nix";
-        rev = "main";  # Use the current commit revision hash
-        hash = "iQU1Rv7Q0BFdsIX9c7mxDhhYaWemuaNRYs+sR1DF0Rc="; # Use the current commit sha256 hash
-      }
-    )).homeManagerModules.${builtins.currentSystem}.default
-  ];
-
 }
