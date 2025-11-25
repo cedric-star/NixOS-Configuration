53eb633e96bafd2b0e7b2366fecbb9f2c9179fbb
cedric-star <cedric-star@gmail.com>
2025-11-25 14:40:09 +0100

    stylix

--- system/boot.nix
+++ system/boot.nix
@@ -7,13 +7,13 @@
     enable = true;
     efiSupport = true;
     device = "nodev";  # Wichtig für EFI-Installation!
-    theme = pkgs.fetchFromGitHub {
-      owner =  "shvchk";
-      repo = "fallout-grub-theme";
-      rev = "master";
-      sha256 = "iQU1Rv7Q0BFdsIX9c7mxDhhYaWemuaNRYs+sR1DF0Rc=";
-      # um hahs herauszufinden: fehlerhaften eintragen, rebuilden, aus fehlermeldug kopieren
-    };
+    #theme = pkgs.fetchFromGitHub {
+    #  owner =  "shvchk";
+    #  repo = "fallout-grub-theme";
+    #  rev = "master";
+    #  sha256 = "iQU1Rv7Q0BFdsIX9c7mxDhhYaWemuaNRYs+sR1DF0Rc=";
+    #  # um hahs herauszufinden: fehlerhaften eintragen, rebuilden, aus fehlermeldug kopieren
+    #};
   };
   
   boot.loader.efi = {
