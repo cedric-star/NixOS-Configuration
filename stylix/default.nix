5ca8f474427100fe6dc88b8ac85342810cf71f2b
cedric-star <cedric-star@gmail.com>
2025-11-26 22:24:46 +0100

    fonting

--- stylix/default.nix
+++ stylix/default.nix
@@ -6,4 +6,12 @@
   stylix.base16Scheme = ./redpink.yaml;
   stylix.polarity = "dark";
 
+  stylix.fonts = {
+    enable = true;
+    monospace = {
+      package = pkgs.nerdfonts.override {fonts = ["JetBrainsmono"];};
+      name = "JetBrainsMono Nerd Font Mono Bold";
+    }
+  };
+
 }
