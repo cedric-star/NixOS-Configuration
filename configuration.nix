b2556fa4c41502831f3f1eae56c5976e31424580
cedric-star <cedric-star@gmail.com>
2025-11-26 09:06:30 +0100

    stylix mod

--- configuration.nix
+++ configuration.nix
@@ -5,31 +5,5 @@
       ./hardware-configuration.nix
       ./system
       ./stylix
-  ]; 
-
-  stylix.enable = true;
-  stylix.image = ./home/gnome/wallpapers/b1.png;
-  stylix.base16Scheme = {
-    base00 = "1a0f14"; # Default Background
-    base00-transparent = "1a0f1480"; # 50% transparent
-
-    
-    base01 = "25181e"; # Lighter Background
-    base02 = "302128"; # Selection Background
-    base03 = "4a3a40"; # Comments
-    base04 = "655359"; # Dark Foreground
-    base05 = "d8c8cf"; # Default Foreground
-    base06 = "e3d8dd"; # Light Foreground
-    base07 = "efe8eb"; # Light Background
-    base08 = "e8b4c9"; # Red/Pink
-    base09 = "e8d4b4"; # Orange/Beige
-    base0A = "f0d8a8"; # Yellow/Warm Beige
-    base0B = "c9e8b4"; # Green
-    base0C = "b4e8d4"; # Cyan
-    base0D = "d4b4e8"; # Blue
-    base0E = "e8b4d4"; # Magenta
-    base0F = "d8a870"; # Brown
-  };
-  stylix.polarity = "dark";
-  
+  ];   
 }
