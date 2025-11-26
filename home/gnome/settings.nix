2187e6f6f88f4e8f8f5d0ea28cd7ed581b9d464a
cedric-star <cedric-star@gmail.com>
2025-11-26 08:53:02 +0100

    templates gnome

--- home/gnome/settings.nix
+++ home/gnome/settings.nix
@@ -17,7 +17,7 @@
     recursive = true;
   };
   home.activation.myCopyTemplates = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    cp $HOME/.local/share/Templates $HOME/Templates
+    cp -r $HOME/.local/share/Templates $HOME/Templates
   '';
 
 
