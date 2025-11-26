9c3ee10c6934662420bd21aad5465b5cead93d4f
cedric-star <cedric-star@gmail.com>
2025-11-26 22:43:09 +0100

    no fonting

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -4,7 +4,7 @@
 
   # home.file.".config/kitty/b1.png".source = ../gnome/wallpapers/b1.png;
   programs.kitty = {
-    enable = true;
+    enable = false;
  
     
     settings = {
@@ -14,6 +14,13 @@
     
     
     shellIntegration.enableFishIntegration = true;
+    #extraConfig = ''
+      # shell ${pkgs.fish}/bin/fish
 
+      #copy_on_select yes
+      
+      #map ctrl+shift+c copy_to_clipboard
+      #map ctrl+shift+v paste_from_clipboard
+    #'';
   };
 }
