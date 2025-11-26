a05b38553601dfeb6dff8c8b7ab4ecb904835c23
cedric-star <cedric-star@gmail.com>
2025-11-26 14:58:39 +0100

    stylix colorscheme

--- stylix/default.nix
+++ stylix/default.nix
@@ -3,24 +3,7 @@
 {
   stylix.enable = true;
   stylix.image = ./wallpapers/b1.png;
-  stylix.base16Scheme = {
-    base00 = "1a0f14"; # Default Background
-    base01 = "25181e"; # Lighter Background
-    base02 = "302128"; # Selection Background
-    base03 = "edd5a5"; # Comments
-    base04 = "655359"; # Dark Foreground
-    base05 = "d8c8cf"; # Default Foreground
-    base06 = "e3d8dd"; # Light Foreground
-    base07 = "efe8eb"; # Light Background
-    base08 = "e8b4c9"; # Red/Pink
-    base09 = "e8d4b4"; # Orange/Beige
-    base0A = "f0d8a8"; # Yellow/Warm Beige
-    base0B = "c9e8b4"; # Green
-    base0C = "b4e8d4"; # Cyan
-    base0D = "d4b4e8"; # Blue
-    base0E = "e8b4d4"; # Magenta
-    base0F = "d8a870"; # Brown
-  };
+  stylix.base16Scheme ./redpink.yaml;
   stylix.polarity = "dark";
   
 
