c5ef48300f5944843003c70f46522304eaca63e9
cedric-star <cedric-star@gmail.com>
2025-11-29 10:39:40 +0100

    openbar

--- home/gnome/keybindings.nix
+++ home/gnome/keybindings.nix
@@ -11,26 +11,26 @@
       
       
 ### move to specific workspace
-      switch-to-workspace-1 = ['<Super>1'];
-      switch-to-workspace-2 = ['<Super>2'];
-      switch-to-workspace-3 = ['<Super>3'];
-      switch-to-workspace-4 = ['<Super>4'];
-      switch-to-workspace-5 = ['<Super>5'];
-      switch-to-workspace-6 = ['<Super>6'];
-      switch-to-workspace-7 = ['<Super>7'];
-      switch-to-workspace-8 = ['<Super>8'];
-      switch-to-workspace-9 = ['<Super>9'];
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
+      switch-to-workspace-5 = ["<Super>5"];
+      switch-to-workspace-6 = ["<Super>6"];
+      switch-to-workspace-7 = ["<Super>7"];
+      switch-to-workspace-8 = ["<Super>8"];
+      switch-to-workspace-9 = ["<Super>9"];
 
 ### move windows to specific workspace
-      move-to-workspace-1 = ['<Alt>1'];
-      move-to-workspace-2 = ['<Alt>2'];
-      move-to-workspace-3 = ['<Alt>3'];
-      move-to-workspace-4 = ['<Alt>4'];
-      move-to-workspace-5 = ['<Alt>5'];
-      move-to-workspace-6 = ['<Alt>6'];
-      move-to-workspace-7 = ['<Alt>7'];
-      move-to-workspace-8 = ['<Alt>8'];
-      move-to-workspace-9 = ['<Alt>9'];
+      move-to-workspace-1 = ["<Alt>1"];
+      move-to-workspace-2 = ["<Alt>2"];
+      move-to-workspace-3 = ["<Alt>3"];
+      move-to-workspace-4 = ["<Alt>4"];
+      move-to-workspace-5 = ["<Alt>5"];
+      move-to-workspace-6 = ["<Alt>6"];
+      move-to-workspace-7 = ["<Alt>7"];
+      move-to-workspace-8 = ["<Alt>8"];
+      move-to-workspace-9 = ["<Alt>9"];
 
 
 ### sachen ausstellen
