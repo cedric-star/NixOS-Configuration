55189dc214b27888fd040800ad0901cbc6790fea
cedric-star <cedric-star@gmail.com>
2025-11-29 10:55:03 +0100

    keybindings

--- home/gnome/keybindings.nix
+++ home/gnome/keybindings.nix
@@ -88,10 +88,42 @@
       toggle-maximized = [];
       toggle-on-all-workspaces = [];
       unmaximize = [];
+    };
 
-      
+    "org/gnome/desktop/wm/keybindings" = {
+### bildschirm aufnehmen
+      screenshot = ["<Shift>Print"];
+      screenshot-window = ["<Alt>Print"];
+      show-screenshot-ui = ["Print"];
       
 
+### sachen ausstellen
+      focus-active-notification = [];
+      open-new-window-application-1 = [];
+      open-new-window-application-2 = [];
+      open-new-window-application-3 = [];
+      open-new-window-application-4 = [];
+      open-new-window-application-5 = [];
+      open-new-window-application-6 = [];
+      open-new-window-application-7 = [];
+      open-new-window-application-8 = [];
+      open-new-window-application-9 = [];
+      shift-overview-down = [];
+      shift-overview-up = [];
+      show-screen-recording-ui = [];
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
+      switch-to-application-9 = [];
+      toggle-application-view = [];
+      toggle-message-tray = [];
+      toggle-overview = [];
+      toggle-quick-settings = [];
 
     };
   };  
