9cbd3ecefcaa93b976aa0844f7cfee9b2e81efc5
cedric-star <cedric-star@gmail.com>
2025-12-09 12:19:03 +0100

    pip

--- system/system_programs/python.nix
+++ system/system_programs/python.nix
@@ -1,25 +1,47 @@
 { config, pkgs, ... }:
 
+let
+  featuretools = pkgs.python3.pkgs.buildPythonPackage rec {
+    pname = "featuretools";
+    version = "1.27.0";
+    format = "setuptools";
+    
+    src = pkgs.fetchPypi {
+      inherit pname version;
+      hash = "sha256-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX="; # Ersetze mit tatsächlichem Hash
+    };
+    
+    propagatedBuildInputs = with pkgs.python3.pkgs; [
+      pandas
+      numpy
+      scipy
+      tqdm
+      click
+      cloudpickle
+      distributed
+      psutil
+      dask
+      pyyaml
+    ];
+    
+    doCheck = false; # Oft bei komplexen Paketen besser
+  };
+  
+  myPython = pkgs.python3.withPackages (ps: with ps; [
+    pandas
+    matplotlib
+    plotly
+    numpy
+    dtw-python
+    sklearn-compat
+    jupyter
+    
+    # Hier fügen wir unser custom Paket hinzu
+    featuretools
+  ]);
+in
 {
   environment.systemPackages = with pkgs; [
-    
-    (python3.withPackages (ps: with ps; [
-      pandas
-      matplotlib
-      plotly
-      numpy
-      dtw-python
-      sklearn-compat
-
-      pip
-      setuptools
-      
-
-      jupyter
-    ]))# pakete in python direkt einbinden
-
+    myPython
   ];
 }
-
-#run:   jupyter notebook --notebook-dir=./
-#dann:  url mit token ganz am anfang aus consolenausgabe in codium einfügen (existing jupyter server)
