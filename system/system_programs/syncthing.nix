c6f5e168adbcc7a1212b86185cd327b7293b03dc
cedric-star <cedric-star@gmail.com>
2025-12-17 13:43:36 +0100

    syncc

--- system/system_programs/syncthing.nix
+++ system/system_programs/syncthing.nix
@@ -1,11 +1,11 @@
 {config, pkgs, ...}:
 
 {
-  #services.syncthing = {
-  #  enable = true;
-  #  group = "cedric";
-  #  user = "cedric";
-  #};
+  services.syncthing = {
+    enable = true;
+    group = "cedric";
+    user = "cedric";
+  };
 
   # sudo systemctl start syncthing@cedric.service
   # http://localhost:8384/ für gui nach
