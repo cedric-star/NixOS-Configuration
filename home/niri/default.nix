5c419cefcdd44c07f397b58ee2057d7c070f68da
cedric-star <cedric-star@gmail.com>
2026-01-12 21:46:52 +0100

    Auto-commit

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -1,31 +1,55 @@
+# home.nix
 { config, lib, pkgs, inputs, ... }:
 
 let
-  # Pakete aus den Inputs extrahieren
-  quickshell-pkg = inputs.quickshell.packages.${pkgs.system}.default;
-  qml-niri-pkg = inputs.qml-niri.packages.${pkgs.system}.default;
+  quickshell-with-niri = inputs.qml-niri.packages.${system}.quickshell;
+  qml-niri-plugin = inputs.qml-niri.packages.${system}.default;
 in
 {
+  imports = [
+  
+  ];
+
   home.packages = with pkgs; [
-    # Über Inputs importierte Pakete
-    quickshell-pkg
-    qml-niri-pkg
+    # Quickshell mit qml-niri Plugin
+    quickshell-with-niri
     
-    # Systempakete
+    # QML-Niri Plugin (falls separat benötigt)
+    qml-niri-plugin
+    
+    # Niri und Wayland
     niri
-    qt6.qtdeclarative
+    niri-effects
+    
+    # Qt für QML/Quickshell
+    qt6.full
     qt6.qtwayland
+    qt6.qtdeclarative
     qt6.qtsvg
+    qt6.qttools
+    
+    # Wayland Tools
     xwayland-satellite
+    waybar
+    swww
+    wofi
+    
+    # Utilities
     swaybg
     libnotify
     pamixer
     wl-clipboard-rs
-    waybar
-    swww
-    wofi
+
   ];
 
+  # Quickshell Service
+  services.quickshell = {
+    enable = true;
+    # Konfigurationsdatei (optional)
+    # config = ./quickshell/main.qml;
+  };
+
+  # Niri Konfiguration
   home.file.".config/niri/config.kdl".source = ./config.kdl;
   home.file.".config/niri/b1.png".source = ../../stylix/wallpapers/b1.png;
 
