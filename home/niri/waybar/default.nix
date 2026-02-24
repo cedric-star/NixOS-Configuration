997b1aa86aa87796adcfd7e7c18208c2930bc7ff
cedric-star <cedric-star@gmail.com>
2026-02-24 17:27:08 +0100

    Auto-commit

--- home/niri/waybar/default.nix
+++ home/niri/waybar/default.nix
@@ -5,57 +5,7 @@
   
     enable = true;
 
-    style = ''
-* {
-    border: none;
-    border-radius: 0;
-    font-family: Roboto, Helvetica, Arial, sans-serif;
-    font-size: 13px;
-    box-shadow: none;
-    background: transparent;
-}
-
-window#waybar {
-    background: rgba(43, 48, 59, 0.0);
-    border-bottom: 3px solid rgba(100, 114, 125, 0.5);
-    color: white;
-}
-
-tooltip {
-  background: rgba(43, 48, 59, 0.5);
-  border: 1px solid rgba(100, 114, 125, 0.5);
-}
-tooltip label {
-  color: white;
-}
-
-#workspaces button {
-    padding: 0 5px;
-    background: transparent;
-    color: white;
-    border-bottom: 3px solid transparent;
-}
-
-#workspaces button.focused {
-    background: #64727D;
-    border-bottom: 3px solid white;
-}
-
-#mode,
-#clock,
-#battery,
-#cpu,
-#memory,
-#network,
-#pulseaudio,
-#temperature {
-    background-color: #27121e;
-    padding: 4px;
-    color: #e8d2b8;
-}
-      
-      
-    '';
+    style = builtins.readFile ./style.css;
 
     settings = [{
       height = 30;
