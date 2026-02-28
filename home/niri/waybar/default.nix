b815cf2ae77c9c738375e9721d5893ff97771fa5
cedric-star <cdrc.wnsch@gmail.com>
2026-02-28 17:39:11 +0100

    Auto-commit

--- home/niri/waybar/default.nix
+++ home/niri/waybar/default.nix
@@ -2,14 +2,13 @@
 
 {
   home.file.".config/waybar/nixos.svg".source = ./nixos.svg;
+  home.file.".config/waybar/config.jsonc".source = ./config.jsonc;
 
   programs.waybar = {
   
     enable = true;
 
     style = builtins.readFile ./style3.css;
-
-    settings = builtins.readFile ./config.jsonc;
 
   };
 }
