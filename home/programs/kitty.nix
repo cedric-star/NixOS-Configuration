61078d4b8751466dbeb757bb1a18a939f29ac73d
cedric-star <cdrc.wnsch@gmail.com>
2026-03-01 12:44:27 +0100

    Auto-commit

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -8,7 +8,7 @@
  
     
     settings = {
-      #background_opacity = 0.92;
+      background_opacity = 0.92;
       hide_window_decorations = "yes";
     };
     
@@ -18,9 +18,14 @@
       # shell ${pkgs.fish}/bin/fish
 
       copy_on_select yes
+      tab_title_template [katze: {index}]
       
       map ctrl+shift+c copy_to_clipboard
       map ctrl+shift+v paste_from_clipboard
+
+      
+      map ctrl+shift+j neighboring_window left
+      map ctrl+shift+odiaeresis neighboring_window right
     '';
   };
 }
