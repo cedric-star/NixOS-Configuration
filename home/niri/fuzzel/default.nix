e50ade6f4d405124b50286389c60fbffc0852588
cedric-star <cdrc.wnsch@gmail.com>
2026-03-17 12:36:04 +0100

    wlogout

--- home/niri/fuzzel/default.nix
+++ home/niri/fuzzel/default.nix
@@ -1,9 +1,9 @@
 { pkgs, inputs, ... }:
 
 {
-  home.file.".config/fuzzel/fuzzel.ini".source = ./fuzzel.ini;
-
-  programs.fuzzel= {
-    enable = true;
+  home.file.".config/fuzzel/fuzzel.ini" = {
+    source = ./dotfiles/fuzzel/fuzzel.ini; # Or wherever your file is
+    force = true; # This will override the module's file
   };
+
 }
