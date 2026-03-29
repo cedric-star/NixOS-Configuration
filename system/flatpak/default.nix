dca4a5d198c411f701fe829d4793aa6a038159bc
cedric-star <cedric-star@gmail.com>
2026-03-29 17:44:07 +0200

    flatpack changend cofngig

--- system/flatpak/default.nix
+++ system/flatpak/default.nix
@@ -15,13 +15,5 @@
 
 
   };
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
-
 
 }
