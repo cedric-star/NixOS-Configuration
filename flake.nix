f36b3640c409c672b646a71a8d59708a356adc11
cedric-star <cedric-star@gmail.com>
2026-03-29 21:15:43 +0200

    qs

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
 
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
   };
