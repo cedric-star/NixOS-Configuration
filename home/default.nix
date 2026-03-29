49eae0749da8635fce99216704404cfa2b113919
cedric-star <cedric-star@gmail.com>
2026-03-29 17:46:36 +0200

    flatpack changend cofngig

--- home/default.nix
+++ home/default.nix
@@ -8,7 +8,7 @@
   ];  
 
   programs.home-manager.enable = true;
-  home.gtk.gtk4.theme = config.gtk.theme;
+  gtk.gtk4.theme = config.gtk.theme;
   # Home Manager needs a bit of information about you and the paths it should
 
   # manage.
