e02b228f9c2d8b82f2d1543fd139481b001211d2
cedric-star <cedric-star@gmail.com>
2026-03-30 12:30:17 +0200

    update

--- system/networking.nix
+++ system/networking.nix
@@ -34,11 +34,7 @@
     };
   };
 
-  nix.settings = {
-    extra-substituters = [ "https://noctalia.cachix.org" ];
-    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-  };
-  
+
   # NUR EINEN VON BEIDEN AKTIVIEREN:
   # Entferne eine der beiden Zeilen:
   
