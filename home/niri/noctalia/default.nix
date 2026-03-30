ac0ff17ee8e8001aaa4dde666b93418f8ada0e1f
cedric-star <cedric-star@gmail.com>
2026-03-30 12:18:16 +0200

    cachix noctalia

--- home/niri/noctalia/default.nix
+++ home/niri/noctalia/default.nix
@@ -6,4 +6,9 @@
   ];
 
   programs.noctalia-shell.enable = true;
+
+  nix.settings = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+  };
 }
