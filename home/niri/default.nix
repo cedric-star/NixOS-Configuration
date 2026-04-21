4098ffe79faf0f8a110ac4e659b98201a9d67c49
cedric-star <cedric-star@gmail.com>
2026-04-21 17:29:31 +0200

    clipboard

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -38,6 +38,8 @@ in {
     udiskie             # USB auto-mount
 
     wl-mirror
+    cliphist:           # Clipboard history backend (required)
+    wl-clipboard:       # Wayland clipboard utilities (required)
 
     #noctalia-shell
     #noctalia-qs
