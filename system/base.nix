827f6384d335776ce322a2397c5522e981734a74
cedric-star <cedric-star@gmail.com>
2026-04-23 10:25:15 +0200

    clipboard

--- system/base.nix
+++ system/base.nix
@@ -65,13 +65,5 @@
   # Polkit wird oft für Berechtigungsdialoge benötigt
   security.polkit.enable = true;
 
-  #für clipboard
-  services.cliphist = {
-    enable = true;
-    allowImages = true;
-    extraOptions = [
-      "-max-items" "20" 
-    ];
-  };
 
 }
