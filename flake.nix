bddc49de1916b0f600e2a0527381fd023f5c7857
cedric-star <cedric-star@gmail.com>
2026-04-23 10:48:03 +0200

    cachsix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
   description = "mynixflake";
+
+  nixConfig = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+  };
  
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -34,10 +39,8 @@
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
-      nix.settings = {
-        extra-substituters = [ "https://noctalia.cachix.org" ];
-        extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-      };
+
+      
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
