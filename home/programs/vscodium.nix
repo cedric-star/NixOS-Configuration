2f72ffdcf4ff503e57a417063dff6c451a7fb13e
cedric-star <cedric-star@gmail.com>
2026-05-07 12:08:58 +0200

    codium

--- home/programs/vscodium.nix
+++ home/programs/vscodium.nix
@@ -17,13 +17,13 @@
   # hier identifier in extension eintragen
       #vscjava.vscode-java-pack              #java
       #ms-vscode.cpptools-extension-pack     #c/c++
-      myriad-dreamin.tinymist               #typst
-      haskell.haskell                       #haskell
-      jnoortheen.nix-ide                    #nix
-      jebbs.plantuml                        #puml
-      mechatroner.rainbow-csv               #csv
-      ms-toolsai.jupyter                    #jupyter
-      ms-python.python                      #python
+      #myriad-dreamin.tinymist               #typst
+      #haskell.haskell                       #haskell
+      #jnoortheen.nix-ide                    #nix
+      #jebbs.plantuml                        #puml
+      #mechatroner.rainbow-csv               #csv
+      #ms-toolsai.jupyter                    #jupyter
+      #ms-python.python                      #python
       #theqtcompany.qt-qml
       #kdl-org.kdl-v1
     ];
