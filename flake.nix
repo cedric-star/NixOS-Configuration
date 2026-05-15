a546ef26bcee549b04b451f1a701654419a45abc
cedric-star <cedric-star@gmail.com>
2026-05-15 22:00:24 +0200

    tabelle5

--- flake.nix
+++ flake.nix
@@ -1,25 +1,26 @@
-{# niri: win plus punkt und komma shortcut
+{
+  # jallo niri: win plus punkt und komma shortcut
   description = "mynixflake";
 
   nixConfig = {
-    extra-substituters = [ 
+    extra-substituters = [
       "https://noctalia.cachix.org"
       "https://cache.nixos.org"
     ];
-    extra-trusted-public-keys = [ 
+    extra-trusted-public-keys = [
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
- 
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-    }; 
-     
+    };
+
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -48,42 +49,46 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-
   };
-  
-  outputs = {self, nvf, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
 
+  outputs =
+    {
+      self,
+      nvf,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
 
-      
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./configuration.nix
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
 
-        {
-          environment.systemPackages = [
-            inputs.ap-visual-sorting.packages.x86_64-linux.default          
-          ];
-        }
+          {
+            environment.systemPackages = [
+              inputs.ap-visual-sorting.packages.x86_64-linux.default
+            ];
+          }
 
+          nvf.nixosModules.default
 
-        nvf.nixosModules.default
-        
-        home-manager.nixosModules.default
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.cedric = import ./home;
-          home-manager.backupFileExtension = "backup";
-          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.nixosModules.default
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.cedric = import ./home;
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = { inherit inputs; };
 
-          home-manager.sharedModules = [ inputs.myvim.homeManagerModules.default ];       
-        }
+            home-manager.sharedModules = [ inputs.myvim.homeManagerModules.default ];
+          }
 
-        inputs.stylix.nixosModules.stylix
-        inputs.nix-flatpak.nixosModules.nix-flatpak
-      ];
+          inputs.stylix.nixosModules.stylix
+          inputs.nix-flatpak.nixosModules.nix-flatpak
+        ];
+      };
     };
-  };
 }
