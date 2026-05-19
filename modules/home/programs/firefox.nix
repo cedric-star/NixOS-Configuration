7e230d7d1b623e501b4000f8d1b8a57cf7646afa
cedric-star <cdrc.wnsch@gmail.com>
2026-05-19 10:54:13 +0200

    firefox path fix

--- modules/home/programs/firefox.nix
+++ modules/home/programs/firefox.nix
@@ -3,7 +3,7 @@
 {
   programs.firefox = {
     enable = true;
-    configPath = "${config.xdg.configHome}/.mozilla/firefox";
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
     languagePacks = [ "de" "en-US" ];
     profiles = {
       default = {
