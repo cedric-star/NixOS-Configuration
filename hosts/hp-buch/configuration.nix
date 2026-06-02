d4f2266aeafed5f9433bffa80ff2138849a7156b
cedric-star <cdrc.wnsch@gmail.com>
2026-06-02 15:54:36 +0200

    notification dinge

--- hosts/hp-buch/configuration.nix
+++ hosts/hp-buch/configuration.nix
@@ -12,5 +12,18 @@
   networking.hostName = "hp-buch";
   system.stateVersion = "25.05"; 
 
+    # Intel Grafik / OpenGL
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # optional: bessere Video-Beschleunigung
+  environment.systemPackages = with pkgs; [
+    intel-media-driver
+    intel-gpu-tools
+    mesa-demos
+    vulkan-tools
+  ];
 
 }
--- modules/theming/default.nix
+++ modules/theming/default.nix
@@ -8,6 +8,7 @@
     pywalfox-native
     gsettings-desktop-schemas
     wallust
+    spicetify-cli
   ];
 
   environment.pathsToLink = [
@@ -17,6 +18,7 @@
   environment.sessionVariables = {
     GTK_THEME = "adw-gtk3";
     GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
+    MOZ_ENABLE_WAYLAND="1";
   };
 
   programs.dconf.enable = true;
